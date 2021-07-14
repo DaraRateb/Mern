651d4ef1a90c18498c1418d7567005568c1d5be1
Dara <dararateb93@gmail.com>
2021-07-14 14:59:04 +0300

    second class assignment

--- React/Class Component/propitup/src/component/PersonCard .js
+++ React/Class Component/propitup/src/component/PersonCard .js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 class PersonCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age,
+        };
+    }
     render() {
         return (
             <malak>
                 <h1>{this.props.firstname}, {this.props.lastname}</h1>
-            <p>Age: {this.props.age} </p>
+            <p>Age: {this.state.age} </p>
             <p>Hair Color: {this.props.color}</p>
+            <button onClick={ ()=> {this.setState({age:this.state.age+1}) }}>Click Me</button>
             </malak>
         );
     }
