0c4d340803eaeaea642c8104485d1bdf2c118a91
Dara <dararateb93@gmail.com>
2021-07-27 15:38:33 +0300

    Luke assignment

--- React/Routing/luke-api/src/App.js
+++ React/Routing/luke-api/src/App.js
@@ -1,11 +1,27 @@
-import Home from './components/Home';
+// import Home from './components/Home';
 import './App.css';
-
+import Component1 from './components/Component1';
+import Form from './components/Form';
+import {useState} from 'react'
 function App() {
   
+  const [returnedTypeVal, setReturnedTypeVal] = useState();
+  const [returnedId, setReturnedId] = useState();
+
+
+  const takeData = (typeVal,id)=>{
+    setReturnedId(id);
+    setReturnedTypeVal(typeVal);
+    
+  }
+
   return (
     <div className="App">
-      <Home path="/"/>
+      {/* <Home path="/"/> */}
+      <Form putData={takeData}/>
+      <Component1 typeVal={returnedTypeVal} cmpId={returnedId}/>
+      
+
     </div>
   );
 }
